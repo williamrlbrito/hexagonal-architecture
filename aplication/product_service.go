413fbe2ba3b2576d7fabf5b890ef94e4f9e463d7
William Brito <williamrldb@gmail.com>
2023-11-26 21:16:26 -0400

    --Creating service and Get method

---
+++ aplication/product_service.go
@@ -0,0 +1,14 @@
+package aplication
+
+type ProductService struct {
+	Persistence ProductPersistenceInterface
+}
+
+func (service *ProductService) Get(id string) (ProductInterface, error) {
+	product, err := service.Persistence.Get(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}

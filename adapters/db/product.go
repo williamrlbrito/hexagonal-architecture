a843082276ba88695ea0828f81d3c00043cccee8
William Brito <williamrldb@gmail.com>
2023-12-05 21:08:31 -0400

    --Starting to create the Db adapter

---
+++ adapters/db/product.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/williamrlbrito/hexagonal-architecture/application"
+)
+
+type ProductDb struct {
+	db *sql.DB
+}
+
+func (productDb *ProductDb) Get(id string) (application.ProductInterface, error) {
+	product := application.Product{}
+	stmt, err := productDb.db.Prepare("select id, name, price, status from products where id = ?")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer stmt.Close()
+
+	if err := stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status); err != nil {
+		return nil, err
+	}
+
+	return &product, nil
+}

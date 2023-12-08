3f215aad0b4a2a7306a3f147df9fe03ba6daee33
William Brito <williamrldb@gmail.com>
2023-12-07 20:35:22 -0400

    --Testing Save method

--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/williamrlbrito/hexagonal-architecture/adapters/db"
+	"github.com/williamrlbrito/hexagonal-architecture/application"
 )
 
 var Db *sql.DB
@@ -50,4 +51,27 @@ func TestProductDb_Get(t *testing.T) {
 	assert.Equal(t, "Product Test", product.GetName())
 	assert.Equal(t, 10.0, product.GetPrice())
 	assert.Equal(t, "enabled", product.GetStatus())
+}
+
+func TestProductDb_Save(t *testing.T) {
+	setUP()
+	defer Db.Close()
+
+	productDb := db.NewProductDb(Db)
+	product := application.NewProduct()
+	product.Name = "Product Test"
+	product.Price = 10
+
+	result, err := productDb.Save(product)
+	assert.Nil(t, err)
+	assert.Equal(t, product.Name, result.GetName())
+	assert.Equal(t, product.Price, result.GetPrice())
+	assert.Equal(t, product.Status, result.GetStatus())
+
+	product.Status = "enabled"
+	result, err = productDb.Save(product)
+	assert.Nil(t, err)
+	assert.Equal(t, product.Name, result.GetName())
+	assert.Equal(t, product.Price, result.GetPrice())
+	assert.Equal(t, product.Status, result.GetStatus())
 }
